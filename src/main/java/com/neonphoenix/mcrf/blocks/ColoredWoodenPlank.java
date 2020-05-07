0d9351571aab6588ee7fd22b9bbd28593a8991ae
NeonPhoenix <neonphoenix.software@gmail.com>
2020-05-07 18:01:26 +1000

    Added colored wooden planks

---
+++ src/main/java/com/neonphoenix/mcrf/blocks/ColoredWoodenPlank.java
@@ -0,0 +1,41 @@
+package com.neonphoenix.mcrf.blocks;
+
+import com.neonphoenix.mcrf.objectholders.ColoredWoodenPlanks;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.DyeColor;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+
+import java.util.stream.Stream;
+
+public class ColoredWoodenPlank extends Block
+{
+    public ColoredWoodenPlank(Material mat, DyeColor dyeColor)
+    {
+        super(Block.Properties.create(mat, dyeColor).hardnessAndResistance(1.8F));
+    }
+
+    public static Stream<Block> allBlocks()
+    {
+        return Stream.of(
+                ColoredWoodenPlanks.WHITE,
+                ColoredWoodenPlanks.ORANGE,
+                ColoredWoodenPlanks.MAGENTA,
+                ColoredWoodenPlanks.LIGHT_BLUE,
+                ColoredWoodenPlanks.YELLOW,
+                ColoredWoodenPlanks.LIME,
+                ColoredWoodenPlanks.PINK,
+                ColoredWoodenPlanks.GRAY,
+                ColoredWoodenPlanks.LIGHT_GRAY,
+                ColoredWoodenPlanks.CYAN,
+                ColoredWoodenPlanks.PURPLE,
+                ColoredWoodenPlanks.BLUE,
+                ColoredWoodenPlanks.BROWN,
+                ColoredWoodenPlanks.GREEN,
+                ColoredWoodenPlanks.RED,
+                ColoredWoodenPlanks.BLACK
+        );
+    }
+}
--- src/main/java/com/neonphoenix/mcrf/init/ModdedBlocks.java
+++ src/main/java/com/neonphoenix/mcrf/init/ModdedBlocks.java
@@ -1,9 +1,12 @@
 package com.neonphoenix.mcrf.init;
 
 import com.neonphoenix.mcrf.MinecraftCuttingRoomFloor;
+import com.neonphoenix.mcrf.blocks.*;
+
 import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.DyeColor;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.ResourceLocation;
@@ -13,7 +16,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.Logger;
 
-import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 import static com.neonphoenix.mcrf.lists.MCRFBlocks.*;
 
@@ -39,14 +42,19 @@ public class ModdedBlocks
         RED_MOSSY_COBBLESTONE_STAIRS = registerStairsBlock(new StairsBlock(() -> RED_MOSSY_COBBLESTONE.getDefaultState(), Block.Properties.create(Material.ROCK).hardnessAndResistance(0.0f, 0.0f).sound(SoundType.STONE)), location("red_mossy_cobblestone_stairs"));
         RED_MOSSY_COBBLESTONE_WALL = registerWallBlock(new WallBlock(Block.Properties.create(Material.ROCK).hardnessAndResistance(0.0f, 0.0f).sound(SoundType.STONE)), location("red_mossy_cobblestone_wall"));
 
-        RED_MOSSY_stone_brick = registerBlock(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(0.0f, 0.0f).sound(SoundType.STONE)), location("red_mossy_stone_brick"));
-        RED_MOSSY_stone_brick_SLAB = registerSlabBlock(new SlabBlock(Block.Properties.create(Material.ROCK).hardnessAndResistance(0.0f, 0.0f).sound(SoundType.STONE)), location("red_mossy_stone_brick_slab"));
-        RED_MOSSY_stone_brick_STAIRS = registerStairsBlock(new StairsBlock(() -> RED_MOSSY_stone_brick.getDefaultState(), Block.Properties.create(Material.ROCK).hardnessAndResistance(0.0f, 0.0f).sound(SoundType.STONE)), location("red_mossy_stone_brick_stairs"));
-        RED_MOSSY_stone_brick_WALL = registerWallBlock(new WallBlock(Block.Properties.create(Material.ROCK).hardnessAndResistance(0.0f, 0.0f).sound(SoundType.STONE)), location("red_mossy_stone_brick_wall"));
+        RED_MOSSY_STONE_BRICK = registerBlock(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(0.0f, 0.0f).sound(SoundType.STONE)), location("red_mossy_stone_brick"));
+        RED_MOSSY_STONE_BRICK_SLAB = registerSlabBlock(new SlabBlock(Block.Properties.create(Material.ROCK).hardnessAndResistance(0.0f, 0.0f).sound(SoundType.STONE)), location("red_mossy_stone_brick_slab"));
+        RED_MOSSY_STONE_BRICK_STAIRS = registerStairsBlock(new StairsBlock(() -> RED_MOSSY_STONE_BRICK.getDefaultState(), Block.Properties.create(Material.ROCK).hardnessAndResistance(0.0f, 0.0f).sound(SoundType.STONE)), location("red_mossy_stone_brick_stairs"));
+        RED_MOSSY_STONE_BRICK_WALL = registerWallBlock(new WallBlock(Block.Properties.create(Material.ROCK).hardnessAndResistance(0.0f, 0.0f).sound(SoundType.STONE)), location("red_mossy_stone_brick_wall"));
 
         //Ore
         NETHER_GOLD_ORE = registerBlock(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(2.0f, 3.0f).sound(SoundType.STONE)), location("nether_gold_ore"));
         RED_MOSS_ORE = registerBlock(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(2.0f, 3.0f).sound(SoundType.STONE)), location("red_moss_ore"));
+
+        //Colored Blocks
+        Stream.of(DyeColor.values()).forEach(dyeColor -> {
+            registerBlock(new ColoredWoodenPlank(Material.WOOD, dyeColor), location(dyeColor + "_wooden_planks"));
+        });
 
         log.info("Blocks registered.");
     }
