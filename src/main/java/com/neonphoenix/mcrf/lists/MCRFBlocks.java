f4ebbbaec4e9a38a6b638eeb3878b5add73dc4c3
NeonPhoenix <neonphoenix.software@gmail.com>
2020-05-07 17:58:43 +1000

    Update MCRFBlocks.java

--- src/main/java/com/neonphoenix/mcrf/lists/MCRFBlocks.java
+++ src/main/java/com/neonphoenix/mcrf/lists/MCRFBlocks.java
@@ -11,10 +11,10 @@ public abstract class MCRFBlocks
     public static Block RED_MOSSY_COBBLESTONE_STAIRS;
     public static Block RED_MOSSY_COBBLESTONE_WALL;
 
-    public static Block RED_MOSSY_stone_brick;
-    public static Block RED_MOSSY_stone_brick_SLAB;
-    public static Block RED_MOSSY_stone_brick_STAIRS;
-    public static Block RED_MOSSY_stone_brick_WALL;
+    public static Block RED_MOSSY_STONE_BRICK;
+    public static Block RED_MOSSY_STONE_BRICK_SLAB;
+    public static Block RED_MOSSY_STONE_BRICK_STAIRS;
+    public static Block RED_MOSSY_STONE_BRICK_WALL;
 
     public static Block NETHER_GOLD_ORE;
     public static Block RED_MOSS_ORE;
